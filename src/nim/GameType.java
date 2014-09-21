9bed5d615a4ee40c8f054d5ea6fdda09202eb169
Tyler Haigh <tylerhaigh1994@me.com>
2014-09-21 18:01:22 +1000

    Allows typecasting from integer to GameType

--- src/nim/GameType.java
+++ src/nim/GameType.java
@@ -1,7 +1,17 @@
 package nim;
 
 public enum GameType {
+	Undefined,
 	YouWin,
 	FairGo,
-	MissionImpossible
+	MissionImpossible;
+	
+	public static GameType FromInteger(int val) {
+		switch (val) {
+			case 1 : return YouWin;
+			case 2 : return FairGo;
+			case 3 : return MissionImpossible;
+			default : return Undefined;
+		}
+	}
 }
