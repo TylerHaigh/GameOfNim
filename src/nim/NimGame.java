1eeba9aa291ff3f28e83b1b658d1963bebf6e096
Tyler Haigh <tylerhaigh1994@me.com>
2014-09-21 17:46:02 +1000

    Adds runnable NimGame class
    NimGame implements runnable to "allow" it to print to the console

---
+++ src/nim/NimGame.java
@@ -0,0 +1,16 @@
+package nim;
+
+public class NimGame implements Runnable {
+
+	private int initialNumMatchsticks;
+	
+	public NimGame(int initialNumMatchsticks) {
+		this.initialNumMatchsticks = initialNumMatchsticks;
+	}
+	
+	@Override
+	public void run() {
+		
+	}
+
+}
