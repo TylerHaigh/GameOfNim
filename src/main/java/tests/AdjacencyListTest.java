ade2ccae54c5cfa9f5f900e2af4cf96eda18639d
Tyler Haigh <tylerhaigh1994@me.com>
2014-10-12 09:55:54 +1100

    Adds Unit Tests for Adjacency List clas

---
+++ src/main/java/tests/AdjacencyListTest.java
@@ -0,0 +1,40 @@
+package tests;
+
+import junit.framework.Assert;
+import nim.*;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AdjacencyListTest {
+
+	@Test
+	public void testGetSize() {
+		AdjacencyList adjList = new AdjacencyList(3);
+		int expectedSize = 3;
+		
+		Assert.assertEquals(expectedSize, adjList.getSize());
+	}
+	
+	@Test
+	public void testAdd() {
+		AdjacencyList adjList = new AdjacencyList(2);
+		Vertex addVertex = new Vertex();
+		adjList.add(addVertex, 0);
+		
+		Assert.assertEquals(addVertex, adjList.getVertex(0, 0));
+		
+	}
+	
+	@Test
+	public void testAdjacentVertices() {
+		AdjacencyList adjList = new AdjacencyList(2);
+		Vertex addVertex = new Vertex();
+		adjList.add(addVertex, 0);
+		adjList.add(addVertex, 0);
+		int expetedSize = 2;
+		
+		Assert.assertEquals(expetedSize, adjList.adjacentVertices(0));
+	}
+}
