d68a84f54b2a7e5c729524171620da4d47c56927
Tyler Haigh <tylerhaigh1994@me.com>
2014-10-15 10:34:03 +1100

    Adds Package info for NIM

---
+++ src/main/java/nim/package-info.java
@@ -0,0 +1,15 @@
+
+/**
+ * NIM (The Subtraction game) is a game involving a pile of n matchsticks on a
+ * table. The first player takes k matchsticks (1 <= k < n). Successive players
+ * may take j sticks (j <= n - k and 1 <= j <= 2k). The last player to take a
+ * matchstick wins.
+ * 
+ * This program was developed as an assignment for COMP2230 - Intro to
+ * Algorithmics at University of Newcastle (2014)
+ * 
+ * @author Tyler Haigh - C3182929
+ * @author Simon Hartcher - C3185790
+ *
+ */
+package nim;
