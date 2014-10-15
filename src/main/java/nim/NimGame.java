033f170fcf639790e36cb394b6290169dfeb90f6
Tyler Haigh <tylerhaigh1994@me.com>
2014-10-15 21:31:45 +1100

    Documents Classes
    Documents NIM Game, NIM Vertex, Vertex

--- src/main/java/nim/NimGame.java
+++ src/main/java/nim/NimGame.java
@@ -3,8 +3,16 @@ package nim;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * Plays the Game of NIM (The Subtraction Game)
+ * 
+ * @author Tyler Haigh - C3182929
+ * @author Simon Hartcher - C3185790
+ *
+ */
 public class NimGame {
 
+	//Private instance variables
 	private int initialNumMatchsticks;
 	private int numSticksLeft;
 	private int lastNumRemoved;
@@ -13,23 +21,36 @@ public class NimGame {
 	private Scanner console;
 	private boolean playersTurn;
 	
+	/**
+	 * Constructor for the NIM Game. Sets the initial state of the game
+	 * 
+	 * @param initialNumMatchsticks The number of sticks to start the game with
+	 */
 	public NimGame(int initialNumMatchsticks) {
 		this.initialNumMatchsticks = initialNumMatchsticks;
 		this.numSticksLeft = initialNumMatchsticks;
 		console = new Scanner(System.in);
 	}
 	
+	/**
+	 * Starts the NIM game
+	 */
 	public void start() {
 		setGameType();
 		playGame();
 	}
 	
+	/**
+	 * Offers the user the choice to set the game type to play with
+	 */
 	private void setGameType() {
+		//Display choices
 		System.out.println("Please select the level:");
 		System.out.println("\t[1] You Win");
 		System.out.println("\t[2] Fair Go");
 		System.out.println("\t[3] Mission Impossible");
 		
+		//Get user input for game type
 		int gameType = console.nextInt();
 		while (gameType < 1 || gameType > 3) {
 			System.out.println("Error: Level is out of bounds");
@@ -37,29 +58,39 @@ public class NimGame {
 			gameType = console.nextInt();
 		}
 		
-		this.gameType = GameType.FromInteger(gameType);
+		//Get the Game type to use when playing
+		this.gameType = GameType.fromInteger(gameType);
 	}
 	
+	/**
+	 * Main game loop for the Game of NIM. Continues playing game until the
+	 * game is over (0 sticks left) or the player chooses to quit (enters -1)
+	 */
 	private void playGame() {
 		System.out.println("To return to the previous menu, enter -1 at any time");
 
 		if (gameType == GameType.FairGo) {
+			//Determine whether the player wants to go first
 			System.out.print("Do you want to go first? [Y|N]: ");
 			
+			//Get the user input
 			console = new Scanner(System.in);
 			String playFirst = console.next();
 			
+			//Perform error checking for invalid entry
 			while (!playFirst.equalsIgnoreCase("Y") && !playFirst.equalsIgnoreCase("N")) {
 				System.out.println("Error: Please enter either Y or N");
 				System.out.print("Do you want to go first? [Y|N]: ");
 				playFirst = console.next();
 			}
 			
+			//Set whose turn it is
 			playersTurn = (playFirst.equalsIgnoreCase("Y")) ? true : false;
 		} else {
 			playersTurn = (gameType == GameType.MissionImpossible) ? false: true;
 		}
 		
+		//Re-initialise the scanner to prevent issues with reading input
 		console = new Scanner(System.in);
 		
 		//TODO: Implement game
@@ -101,6 +132,12 @@ public class NimGame {
 		
 	}
  	
+	/**
+	 * Calculates the number of sticks the computer will take based on the Game
+	 * Type as chosen by the player
+	 * 
+	 * @return The number of sticks the computer will remove
+	 */
 	private int getComputerMove() {
 		Random rand = new Random();
 		
@@ -110,22 +147,38 @@ public class NimGame {
 		} else if (gameType == GameType.YouWin) {
 			//Send player to winning position
 		} else {
-			//Play fair?
+			//Play fair
+			
+			if (numSticksLeft == initialNumMatchsticks) {
+				return rand.nextInt(initialNumMatchsticks);
+			} else {
+				int upperBound = Math.min(lastNumRemoved * 2, numSticksLeft);
+				return rand.nextInt(upperBound);
+			}
 		}
 		
-		if (numSticksLeft == initialNumMatchsticks) {
-			return rand.nextInt(initialNumMatchsticks);
-		} else {
-			int upperBound = Math.min(lastNumRemoved * 2, numSticksLeft);
-			return rand.nextInt(upperBound);
-		}
+		return 1; //TODO: Remember to remove this after implementation
 	}
 	
+	/**
+	 * Validates a move as made by the computer or player
+	 *
+	 * @param numSticksToRemove The number of sticks attempting to remove
+	 * @return Whether removing the input number of sticks is a valid move or not
+	 */
 	private boolean validMove(int numSticksToRemove) {
-		int upperBound = Math.min(lastNumRemoved * 2, numSticksLeft);
+		
+		int upperBound = (numSticksLeft == initialNumMatchsticks) ?
+				initialNumMatchsticks - 1
+				: Math.min(lastNumRemoved * 2, numSticksLeft);
+		
 		return numSticksToRemove <= upperBound;
 	}
 	
+	/**
+	 * Checks whether the game has been won
+	 * @return Whether the game is over or not
+	 */
 	private boolean gameOver() {
 		return numSticksLeft == 0;
 	}
--- src/main/java/nim/Vertex.java
+++ src/main/java/nim/Vertex.java
@@ -3,6 +3,13 @@ package nim;
 import java.util.Iterator;
 import java.util.LinkedList;
 
+/**
+ * Represents a Vertex in a Graph
+ * 
+ * @author Tyler Haigh - C3182929
+ * @author Simon Hartcher - C3185790
+ *
+ */
 public class Vertex {
 	
 	//Private static instance variables
@@ -16,61 +23,110 @@ public class Vertex {
 	
 	//Constructors
 	
+	/**
+	 * Constructor for a Vertex. Sets the instance data
+	 */
 	public Vertex() {
+		//Set the private instance variables
 		this.adjacentVertices = new LinkedList<Vertex>();
 		this.index = _index;
 		this.marked = false;
 		
+		//Increment the static index
 		_index++;
 	}
 		
 	//Getters
 	
+	/**
+	 * Getter method for the List of Adjacent Vertices to this Vertex
+	 * @return A LIst of Vertices adjacent to this Vertex
+	 */
 	public LinkedList<Vertex> getAdjacentVertices() {
 		return adjacentVertices;
 	}
 
+	/**
+	 * Getter method for the index of this Vertex in the Graph
+	 * @return The index of this Vertex
+	 */
 	public int getIndex() {
 		return index;
 	}
 	
+	/**
+	 * Getter method for whether this Vertex has been marked or visited. Used
+	 * when traversing a Graph
+	 * 
+	 * @return True if this Vertex is marked, false otherwise
+	 */
 	public boolean isMarked() {
 		return marked;
 	}
 
 	//Setters
 
+	/**
+	 * Setter method for the Adjacent Vertices to this Vertex
+	 * @param adjacentVertices The new list of Adjacent Vertices
+	 * @warning This will break an existing Graph
+	 */
 	public void setAdjacentVertices(LinkedList<Vertex> adjacentVertices) {
 		this.adjacentVertices = adjacentVertices;
 	}
 
+	/**
+	 * Setter method for the index of this Vertex within the Graph
+	 * @param index The new index of this Vertex
+	 */
 	public void setIndex(int index) {
 		this.index = index;
 	}
 
+	/**
+	 * Setter method for whether this Vertex has been marked. Used when
+	 * traversing a Graph
+	 * 
+	 * @param marked True if the Vertex is to be marked, false otherwise
+	 */
 	public void setMarked(boolean marked) {
 		this.marked = marked;
 	}
 	
 	//Queries
 	
+	/**
+	 * Appends a new Vertex to the list of adjacent Vertices
+	 * @param v The Vertex to become adjacent to this Vertex
+	 */
 	public void append(Vertex v) {
 		adjacentVertices.add(v);
 	}
 
+	/**
+	 * Checks whether two Vertices are equal
+	 * @param obj A generic object to test against
+	 * @return True if the Vertices are equal, false otherwise
+	 */
 	@Override
 	public boolean equals(Object obj) {
+		//Check the actual references of the objects
 		if (this == obj)
 			return true;
 		
+		//Check the classes match
 		if (obj == null || this.getClass() != obj.getClass())
 			return false;
 
 		Vertex other = (Vertex) obj;
+		
+		//Check that the Adjacent Vertices are equal
 		if (this.adjacentVertices != null && other.adjacentVertices != null) {
+			//Check the size is equal
 			if (adjacentVertices.size() != other.adjacentVertices.size()) {
 				return false;
 			} else {
+				//Iterate over each Vertex and confirm they are equal
 				Iterator<Vertex> thisIt = this.adjacentVertices.iterator();
 				Iterator<Vertex> otherIt = other.adjacentVertices.iterator();
 				
@@ -89,6 +145,7 @@ public class Vertex {
 			return false;
 		}
 		
+		//All tests have passed, return true
 		return true;
 	}
 }
